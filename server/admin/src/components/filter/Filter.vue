b2307f0c0728aeacb2207128784790d86941c15b
masterit15 <masterit15@yandex.ru>
2022-05-05 08:10:10 +0300

    catalog

--- server/admin/src/components/filter/Filter.vue
+++ server/admin/src/components/filter/Filter.vue
@@ -1,13 +1,87 @@
 <template>
   <div id="filter">
-    <input type="text" name="name" id="filter_name">
-    <input type="date" name="date" id="filter_date">
-    <select name="sort" id="filter_sort">
-      <option value="#">Сортировать по</option>
-      <option value="#">По рейтингу</option>
-      <option value="#">По отзывам</option>
-      <option value="#">По цене</option>
-      <option value="#">По цене</option>
-    </select>
+    <input
+      type="text"
+      v-model="params.name"
+      name="name"
+      id="filter_name"
+      placeholder="По названию"
+    />
+    <input
+      type="text"
+      name="date"
+      id="filter_date"
+      placeholder="По дате (от - до)"
+    />
+    <div class="sort" id="filter_sort">
+      <span class="sort_value" @click="isShow = !isShow">Сортировать по</span>
+      <ul class="sort_list" v-show="isShow">
+        <li class="sort_list_item" @click="sortBy($event, 'by_rating')">
+          По рейтингу
+        </li>
+        <li class="sort_list_item" @click="sortBy($event, 'by_accounts')">
+          По отзывам
+        </li>
+        <li class="sort_list_item" @click="sortBy($event, 'by_price_up')">
+          По цене <i class="fa fa-angle-up"></i>
+        </li>
+        <li class="sort_list_item" @click="sortBy($event, 'by_price_down')">
+          По цене <i class="fa fa-angle-down"></i>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
+<script>
+import AirDatepicker from "air-datepicker";
+export default {
+  data() {
+    return {
+      isShow: false,
+      params: {
+        name: "",
+        dateFrom: "",
+        dateTo: "",
+        sortBy: "",
+      },
+    };
+  },
+  watch: {
+    params:{
+      handler(val){
+       this.$emit('BackParams', val)
+     },
+     deep: true
+    }
+  },
+  mounted() {
+    const that = this;
+    new AirDatepicker("#filter_date", {
+      range: true,
+      timepicker: true,
+      multipleDatesSeparator: " - ",
+      onSelect({ date, formattedDate }) {
+        if (formattedDate.length > 1) {
+          that.params.dateFrom = formattedDate[0];
+          that.params.dateTo = formattedDate[1];
+        } else {
+          that.params.dateFrom = formattedDate[0];
+        }
+      },
+    });
+  },
+  methods: {
+    sortBy(event, value) {
+      let parent = event.target.closest(".sort");
+      let sortValue = parent.querySelector(".sort_value");
+      let allItems = parent.querySelectorAll(".sort_list_item");
+      allItems.forEach((item) => item.classList.remove("active"));
+      event.target.classList.add("active");
+      let thisValue = event.target.innerHTML;
+      sortValue.innerHTML = thisValue;
+      this.params.sortBy = value;
+      this.isShow = false;
+    },
+  },
+};
+</script>
--- server/admin/src/views/CatalogView.vue
+++ server/admin/src/views/CatalogView.vue
@@ -1,24 +1,67 @@
 <template>
   <div class="view_catalog">
     <div class="catalog_head">
-    <h3 class="page_title">{{pageNow}}</h3>
-    <Filter/>
-    <button class="btn_view_all">Все<i class="fa fa-chevron-right" aria-hidden="true"></i></button>
+      <div class="catalog_head_start">
+        <h3 class="page_title">{{ pageNow }}</h3>
+        <button class="btn_filter" @click="isShow = !isShow">
+          <i class="fa fa-sliders" aria-hidden="true"></i>
+        </button>
+      </div>
+      <div class="catalog_head_mid">
+        <Filter v-show="isShow" v-on:back-params="filtered" />
+      </div>
+      <div class="catalog_head_start">
+        <button class="btn_view_all">
+          Все<i class="fa fa-chevron-right" aria-hidden="true"></i>
+        </button>
+      </div>
+    </div>
+    <div class="catalog_list">
+      <div class="catalog_list_item">
+        <span class="catalog_list_item_status"><i class="fa fa-fire"></i></span>
+        <img :src="require('../assets/img/salad.png')" alt="" class="catalog_list_item_media" />
+        <div class="catalog_list_item_content">
+          <h3 class="catalog_list_item_title">Салат с семгой</h3>
+          <p class="catalog_list_item_desc">Далеко-далеко, за словесными горами в стране гласных и согласных живут рыбные тексты. Живет если имени строчка своих.</p>
+          <div class="catalog_list_item_footer">
+            <h2 class="catalog_list_item_price"></h2>
+            <div class="catalog_list_item_action">
+              <span class="catalog_list_item_action_delete"></span>
+              <input
+                type="text"
+                name="count"
+                class="catalog_list_item_action_count"
+              />
+              <span class="catalog_list_item_action_add"></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import {} from 'vuex'
-import Filter from '@/components/filter/Filter'
+import {} from "vuex";
+import Filter from "@/components/filter/Filter";
 export default {
-  name: 'Catalog',
+  name: "Catalog",
+  data() {
+    return {
+      isShow: false,
+    };
+  },
   components: {
-    Filter
+    Filter,
   },
   computed: {
-    pageNow(){
-      return this.$route.name
-    }
-  }
-}
+    pageNow() {
+      return this.$route.name;
+    },
+  },
+  methods: {
+    filtered(params) {
+      console.log("filtered", params);
+    },
+  },
+};
 </script>
